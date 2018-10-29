c00f6ed279c026a163765e3375b672336123be07
licai <licai@mogujie.com>
2018-10-29 09:18:30 +0800

    feat: 群聊欢迎语

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const {
     Wechaty
 } = require('wechaty') // import { Wechaty } from 'wechaty'
 import resource from './resource'
+import welcome from './welcome'
 
 const bot = Wechaty.instance() // Global Instance
 
 bot
     .on('scan', scan)
     .on('login', login)
+    .on('room-join', roomJoin)
     .on('message', message)
     .start()
 
@@ -28,12 +30,6 @@ async function message(msg) {
     const content = msg.content();
     const room = msg.room();
 
-    // console.log(concat.name())
-    // if(concat.name() === '香ing') {
-    //     concat.say("你是傻逼!");
-    //     return;
-    // }
-
     const roomTopic = await room.topic();
 
     // 如果调用 call:react 就返回 react 资料
@@ -51,5 +47,20 @@ async function message(msg) {
 
     if (room) {
         console.log(`Room:${await room.topic()} Concact ${concat.name() }Content: ${content}`)
+    }
+}
+
+/**
+ * @description 有人加入群聊的事件
+ * @param {*} room
+ * @param {被邀请人的列表} inviteeList 
+ * @param {邀请人} inviter
+ */
+async function roomJoin(room, inviteeList, inviter) {
+    const roomTopic = await room.topic();
+    if(roomTopic === '前端桃园'){
+        inviteeList.forEach(c => {
+            await room.say(welcome.data, c)
+        });
     }
 }
--- resource.js
+++ resource.js
@@ -1,7 +1,15 @@
 const resource = {
     react: ` react海量资源:https://github.com/enaqx/awesome-react
         react.js 中文论坛:http://www.react-china.org/
-        react 小书：http://huziketang.mangojuice.top/books/react/`,
+        react 小书：http://huziketang.mangojuice.top/books/react/
+        ReactJS中文视频教程: http://react-china.org/t/reactjs/584
+        React 技术栈系列教程: http://www.ruanyifeng.com/blog/2016/09/react-technology-stack.html
+        百度母婴技术团队—基于Reactjs实现webapp: https://github.com/my-fe/wiki/issues/1
+        React Router 中文文档: https://react-guide.github.io/react-router-cn/
+        react+redux渲染性能优化原理: http://foio.github.io/react-redux-performance-boost/
+        React同构直出总结: http://www.alloyteam.com/2016/06/react-isomorphic/
+        学习Redux刷新了我的前端App状态管理观: https://www.jianshu.com/p/94d8f8a36ab0
+        `,
     node: `
         Node.js 包教不包会:https://github.com/alsotang/node-lessons
         从零开始nodejs系列文章:http://blog.fens.me/series-nodejs/
@@ -12,7 +20,40 @@ const resource = {
          复杂应用的 CSS 性能分析和优化建议：http://www.orzpoint.com/profiling-css-and-optimization-notes/
          web前端性能优化进阶路：http://www.aliued.cn/2013/01/20/web%E5%89%8D%E7%AB%AF%E6%80%A7%E8%83%BD%E4%BC%98%E5%8C%96%E8%BF%9B%E9%98%B6%E8%B7%AF.html
          页面加载中的图片性能优化：https://www.w3ctech.com/topic/776
+     `,
+     vue: `
+        vue 官网：https://cn.vuejs.org/index.html
+        vue 快速入门: https://segmentfault.com/a/1190000003968020
+        Vue.js 源码学习笔记: http://jiongks.name/blog/vue-code-review
+        VUE 源码分析: http://www.cnblogs.com/sskyy/p/3695003.html
+        vue 源码分析之如何实现 observer 和 watcher: https://segmentfault.com/a/1190000004384515
+        Vue相关开源项目库汇总: https://github.com/opendigg/awesome-github-vue
+        Vue技术内幕(强烈推荐): http://hcysun.me/vue-design/art/
+        Vue.js 源码解析: https://github.com/answershuto/learnVue
+     `,
+     面试: `
+        前端工作面试问题: https://github.com/h5bp/Front-end-Developer-Interview-Questions/tree/master/Translations/Chinese
+        前端面试问题集锦: https://github.com/helloqingfeng/Awsome-Front-End-learning-resource/tree/master/21-Front-end-Interview-questions
+        收集的前端面试题和答案: https://github.com/helloqingfeng/Awsome-Front-End-learning-resource/tree/master/23-FE-interview-master
+        50道 CSS 基础面试题（附答案）: https://www.itcodemonkey.com/article/2853.html
+        掘金面试文章精选合集: https://www.jianshu.com/p/cfdf65b7bd89
+        InterviewMap(强烈推荐): https://yuchengkai.cn/docs/zh/frontend/
+     `,
+     前端: `
+        前端吐血推荐的必读书籍: https://mp.weixin.qq.com/s/ZYzXnhHdytEmu7G7Ba0Rmw
+        前端学习路径(余博伦): https://zhuanlan.zhihu.com/p/21935921 
+        javaScript 全栈工程师培训教程: https://github.com/helloqingfeng/Awsome-Front-End-learning-resource/tree/master/30-jstraining
+        前端开发者手册: https://dwqs.gitbooks.io/frontenddevhandbook/content/
+        最全的资源教程——前端涉及的所有知识体系: https://github.com/helloqingfeng/Awsome-Front-End-learning-resource/tree/master/04-Front-end-tutorial-master
+        大前端工具集: https://github.com/helloqingfeng/Awsome-Front-End-learning-resource/tree/master/28-fetool-master
+        javaScript 资源大全中文版: https://github.com/helloqingfeng/Awsome-Front-End-learning-resource/tree/master/31-awesome-javascript-cn
+        前端开发所使用语言的主流学习资源汇集指南: https://github.com/helloqingfeng/Awsome-Front-End-learning-resource/tree/master/07-Front-End-Develop-Guide-master
+        GitHub上值得关注的前端项目: https://github.com/helloqingfeng/Awsome-Front-End-learning-resource/tree/master/08-github-FE-project-master
+        前端开发相关的优秀网站、博客、以及活跃开发者: https://github.com/helloqingfeng/Awsome-Front-End-learning-resource/tree/master/09-front-end-collect-master
+        关于程序员读书-大牛们的书单汇总: https://github.com/helloqingfeng/Awsome-Front-End-learning-resource/tree/master/11-about-reading
+        Web前端开发规范文档: https://github.com/helloqingfeng/Awsome-Front-End-learning-resource/tree/master/25-web-develop-standard-master
      `
+
 }
 
 export default resource;
